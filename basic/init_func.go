0ef8d86dbcbbe273bbf19071207200ff0d06f056
hooopp <pooh053715717@gmail.com>
2025-08-01 10:21:59 +0700

    feat: init_func.go

---
+++ basic/init_func.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func init() {
+	fmt.Println("Initialization function called")
+}
+
+func init() {
+	fmt.Println("Another initialization function called")
+}
+
+func main() {
+	fmt.Println("Main function called")
+}
