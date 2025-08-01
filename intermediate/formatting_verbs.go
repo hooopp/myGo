a8541d5336f9331ae7dab1ec551fd83662ea32f6
hooopp <pooh053715717@gmail.com>
2025-08-01 11:29:14 +0700

    feat: formatting_verbs.go

---
+++ intermediate/formatting_verbs.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+func main() {
+	i := 3_215.5
+	string := "Hello, World!"
+	fmt.Printf("%v\n", i)
+	fmt.Printf("%#v\n", i)
+	fmt.Printf("%T\n", i)
+	fmt.Printf("%v%%\n", i)
+
+	fmt.Printf("%s\n", string)
+	fmt.Printf("%#v\n", string)
+	fmt.Printf("%T\n", string)
+
+	
+
+}
